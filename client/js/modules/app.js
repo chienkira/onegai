8cb1de737c887d6510e8740824eeaa2a79ba9033
chiennd <chiennd@rikkeisoft.com>
2016-07-20 14:08:18 +0900

    convert back to html (from php)

--- client/js/modules/app.js
+++ client/js/modules/app.js
@@ -28,19 +28,19 @@ angular
 		$stateProvider
 			.state('home', {
 				url: '/',
-				templateUrl: 'client/pages/home.html',
+				templateUrl: 'pages/home.html',
 				controller: 'homeCtrl',
 				controllerAs: 'home'
 			})
 			.state('ask', {
 				url: '/questions/ask',
-				templateUrl: '/pages/ask.html',
+				templateUrl: 'pages/ask.html',
 				controller: 'askCtrl',
 				controllerAs: 'askModel'
 			})
 			.state('questions', {
 				url: '/questions/:id',
-				templateUrl: '/pages/question.html',
+				templateUrl: 'pages/question.html',
 				controller: 'answerCtrl',
 				controllerAs: 'answer',
 				resolve: {
@@ -52,13 +52,13 @@ angular
 			})
 			.state('tags', {
 				url: '/tags',
-				templateUrl: '/pages/tags.html',
+				templateUrl: 'pages/tags.html',
 				controller: 'tagsCtrl',
 				controllerAs: 'tagsModel'
 			})
 			.state('users', {
 				url: '/users',
-				templateUrl: '/pages/users.html',
+				templateUrl: 'pages/users.html',
 				controller: 'usersCtrl',
 				controllerAs: 'usersModel',
 				resolve: {
